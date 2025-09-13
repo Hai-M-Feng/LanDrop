81d96c194e73f2d31663b4a786cd59d45a9ffc1c
HaiMFeng <2124267796@qq.com>
2025-09-14 00:13:47 +0800

    进行类功能分类

---
+++ src/main/java/haimfeng/landrop/application/MainStart.java
@@ -0,0 +1,7 @@
+package haimfeng.landrop.application;
+
+public class MainStart {
+    public static void main(String[] args) {
+        MainApplication.main(args);
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,16 @@
 module haimfeng.landrop {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.common;
 
+    // 导出包含实际类的包
+    exports haimfeng.landrop.application;
+    exports haimfeng.landrop.controller;
 
+    // 开放包以便FXMLLoader可以反射访问
+    opens haimfeng.landrop.application to javafx.fxml;
+    opens haimfeng.landrop.controller to javafx.fxml;
+
+    // 开放资源包（即使没有类文件）
     opens haimfeng.landrop to javafx.fxml;
-    exports haimfeng.landrop;
 }
