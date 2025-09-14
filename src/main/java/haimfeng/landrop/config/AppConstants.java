83f4ff798b0453ab347706ab34041214c3c9ae41
HaiMFeng <2124267796@qq.com>
2025-09-14 20:12:35 +0800

    添加常量表

---
+++ src/main/java/haimfeng/landrop/config/AppConstants.java
@@ -0,0 +1,19 @@
+package haimfeng.landrop.config;
+
+public final class AppConstants {
+    // App 基本信息
+    public static final String APP_NAME = "LanDrop";
+    public static final String APP_VERSION = "1.0.0";
+    public static final String APP_AUTHOR = "HaiMFeng";
+    public static final String APP_URL = "https://github.com/HaiMFeng/LanDrop";
+
+    // Debug 模式
+    public static final boolean DEBUG = true;
+
+    // 连接配置
+    public static final String LOCAL_IP = "127.0.0.1";
+    public static final String BROADCAST_IP = "255.255.255.255";
+    public static final int LOCAL_PORT = 8888;
+    public static final int BROADCAST_PORT = 8888;
+
+}
