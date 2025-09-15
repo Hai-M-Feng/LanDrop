7090829fcd25ba410741427a234c6ad9e5110045
HaiMFeng <2124267796@qq.com>
2025-09-15 22:39:26 +0800

    修改事件成员

--- src/main/java/haimfeng/landrop/event/BroadcastReceivedEvent.java
+++ src/main/java/haimfeng/landrop/event/BroadcastReceivedEvent.java
@@ -5,7 +5,7 @@ import haimfeng.landrop.service.BroadcastManager.BroadcastPacket;
 public class BroadcastReceivedEvent extends AppEvent{
     public BroadcastPacket broadcastPacket;
     public BroadcastReceivedEvent(BroadcastPacket broadcastPacket) {
-        super("");
+        super("Broadcast received");
         this.broadcastPacket = broadcastPacket;
     }
 }
